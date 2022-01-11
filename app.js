6d727375803bf7a615b6cf4552d717f8c6bd9658
JonaRiosBrambila <95506962+JonaRiosBrambila@users.noreply.github.com>
2022-01-11 16:58:22 -0600

    Tipos de Variables

---
+++ app.js
@@ -0,0 +1,296 @@
+/*
+var x = true;
+console.log(typeof(x))
+
+var x =5;
+var x =5;
+var x =6;
+console.log(x==y);
+
+var pan = ['cuernito', 'piedra', 'polvoron'];
+console.log(pan[1]);
+  
+//Objeto Describir 
+var auto = {marca:"Ford",
+            cilindros:8 ,
+            transmision:"automatico",
+            tapiz:{color:"beige",
+            textura:"fibrosa", material:"sintetico"} 
+        };
+//Arreglo o matriz
+var frutas={tamaño:"pequeña", color:"roja",semillas:"si"};
+var verduras = ["zanahorias", "espinazo", "apio"];
+
+console.log(auto.tapiz.color);
+console.log
+
+var x = 34;
+var y = String (234+123);
+
+console.log(typeof(String(y)));
+
+var x = 34;
+x.toString();
+console.log (typeof(x.toString()));
+
+var y = (234+123).toString();
+var z = (y+3);
+console.log((z));
+
+var x = 3.04040495599495445
+var y = x.toExponential();
+console.log(y);
+
+var x = 3.2404343020302323
+var y = x.toFixed(2);
+console.log(y);
+
+var x = 3.2404343020302323
+var y = x.toPrecision(3);
+console.log(y);
+
+var x = 123;
+
+var y = x.valueOf();
+console.log(typeof(y));
+
+
+var x= "Jhon";
+console.log(x);
+console.log(typeof(x));
+
+var y = Number(x);
+console.log(y);
+console.log(typeof(y));
+
+var x= Number.MAX_VALUE;
+console.log(x);
+console.log(typeof(x));
+
+var y = parseInt(x);
+
+console.log(y);
+console.log(typeof(x));
+
+
+var x = "5";
+
+console.log(x);
+console.log(typeof(x));
+
+var y = +x;
+
+console.log(y);
+console.log(typeof(y));
+
+
+var resultado = "Jhon" + 2;
+console.log(resultado);
+    console.log(typeof(resultado));
+
+var myvar = [1,2,3,4,5];
+console.log(myvar);
+console.log(typeof(myVar));
+
+var cadenita = myvar.toString();
+console.log(cadenita);
+console.log(typeof(cadenita));
+
+var myvar =2 ;
+console.log(myvar);
+console.log(typeof(myVar));
+var cadenita = myvar.toString();
+console.log(cadenita);
+console.log(typeof(cadenita));
+*/ 
+//Part 1
+var x = false;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+console.log(typeof(x));
+
+
+var x = true;
+console.log(x);
+var y = Number(x)
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z))
+
+console.log(typeof(x));
+
+
+var x = 0;
+console.log(x);
+console.log(typeof(x));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = 1;
+console.log(x);
+console.log(typeof(x));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = "0";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = "000";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = "1";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = NaN;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = Infinity;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = -Infinity;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = "";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = "20";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = null;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = undefined;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+//Part 2 Calculator
+
+var birthYear = 1984;
+var futureYear  = 2012;
+var age  = futureYear - birthYear;
+console.log('I will be either ' + age + ' or ' + (age - 1));
+
+
+// Information Card
+
+var name= "Arturo";
+var locati = "cdmx";
+var hobby = " Dance";
+
+console.log("Hi, my name is "+ name + ". I live in " +locati+ " and enjoy "+ hobby);
